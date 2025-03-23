060bb4261d24d1ef8875892df2d843f0b32938f7
Matheus Vieira Nascimento Oliveira <matvno@gmail.com>
2025-03-23 13:58:15 -0300

    feat(frontend): adiciona botão de chat flutuante ao layout

--- frontend/src/app/(paginas)/layout.tsx
+++ frontend/src/app/(paginas)/layout.tsx
@@ -1,3 +1,10 @@
+import BotaoChat from "@/components/chat/BotaoChat"
+
 export default function Layout(props: { children: React.ReactNode }) {
-	return <div>{props.children}</div>
+	return (
+		<div>
+			{props.children}
+			<BotaoChat />
+		</div>
+	)
 }
